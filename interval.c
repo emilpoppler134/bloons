8c4359484345df0b4e653eea6720e480a3e48717
Emil Poppler <emilpoppler@Emils-Air.lan>
2023-10-08 00:59:50 +0200

    Fix the time interval ms bug

--- interval.c
+++ interval.c
@@ -5,26 +5,26 @@
 
 // Initialize a time interval with a specified interval
 time_interval_t init_time_interval(double seconds) {
-  time_t last_time;
-  time(&last_time);
-  double last_time_double = (double)last_time;
-
-  time_interval_t interval;
-  interval.last_time = last_time_double;
-  interval.interval = seconds;
-  return interval;
+  time_interval_t timer;
+  timer.last_time = 0.0;
+  timer.interval = seconds;
+  return timer;
 }
 
 // Check if the time interval has elapsed and reset the timer if it has
 bool check_time_interval(time_interval_t *timer) {
-  time_t current_time;
-  time(&current_time);
-  double current_time_double = (double)current_time;
-  
-  if (current_time_double - timer->last_time >= timer->interval) {
-    timer->last_time = current_time_double;
-    return true;
+  // Get the current time in seconds since the epoch.
+  struct timespec current_time;
+  clock_gettime(CLOCK_REALTIME, &current_time);
+
+  // Calculate the elapsed time in seconds.
+  double elapsed_time = current_time.tv_sec + current_time.tv_nsec / 1e9 - timer->last_time;
+
+  // Check if the elapsed time is greater than or equal to the interval.
+  if (elapsed_time >= timer->interval) {
+    timer->last_time = current_time.tv_sec + current_time.tv_nsec / 1e9;
+    return true; // Interval has passed
+  } else {
+    return false; // Interval has not passed yet
   }
-  
-  return false;
 }
